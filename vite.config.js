886aefc367c4f68f5b0aed6b6fb52a88b366d201
Lotoz <zamira.grippi@gmail.com>
2026-05-31 22:50:45 +0200

    Fix: Vite and dev

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-// Importa el plugin condicionalmente para que no explote si no se necesita
-const electron = process.env.VERCEL ? null : require('vite-plugin-electron/simple')
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    // Solo añadimos el plugin de electron si NO estamos en Vercel
-    !process.env.VERCEL && electron({
-      main: { entry: 'electron/main.js' },
-    })
-  ].filter(Boolean), // Esto elimina los "null" si estamos en Vercel
-
-  // Añade esto para evitar errores de compilación web
+  plugins: [vue()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   }
-})
+});
